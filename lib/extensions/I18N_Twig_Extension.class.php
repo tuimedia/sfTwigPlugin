4a49cb167c08142324015c71014764e344af24d3
henrikbjorn <henrik@bearwoods.dk>
2010-01-09 19:13:15 +0100

    Added a "t" and "__" filter to the i18n twig_extension

--- lib/extensions/I18N_Twig_Extension.class.php
+++ lib/extensions/I18N_Twig_Extension.class.php
@@ -4,6 +4,7 @@ class I18n_Twig_Extension extends Twig_Extension
   public function getFilters()
   {
     return array(
+              't' => new Twig_Filter_Function('__'),
               'format_number_choice' => new Twig_Filter_Function('format_number_choice'),
               'format_country' => new Twig_Filter_Function('format_country'),
               'format_language' => new Twig_Filter_Function('format_language'),
