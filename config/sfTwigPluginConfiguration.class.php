4dfa879c7e47f1d71287a90ea204f04ad238ce8d
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-02-23 02:24:54 +0800

    Removed old SVN tag

--- config/sfTwigPluginConfiguration.class.php
+++ config/sfTwigPluginConfiguration.class.php
@@ -6,7 +6,6 @@
  * @package     sfTwigPlugin
  * @subpackage  config
  * @author      Your name here
- * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
  */
 class sfTwigPluginConfiguration extends sfPluginConfiguration
 {
