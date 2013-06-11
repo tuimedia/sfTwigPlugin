2a62f3b14407a176bc68594407535e09afbaf306
Yuriy Berest <djua.com@gmail.com>
2013-06-11 18:58:00 -0400

    Исправление Url_Twig_Extension, Asset_Twig_Extension. Теперь это не фильтры, а ф-и

--- lib/extensions/Url_Twig_Extension.class.php
+++ lib/extensions/Url_Twig_Extension.class.php
@@ -18,23 +18,25 @@
  */
 class Url_Twig_Extension extends Twig_Extension
 {
-  public function getFilters()
+  public function getFunctions()
   {
+    $options = array('is_safe' => array('html'));
+
     return array(
-      'link_to2'       => new Twig_Filter_Function('link_to2'),
-      'link_to1'       => new Twig_Filter_Function('link_to1'),
-      'url_for2'       => new Twig_Filter_Function('url_for2'),
-      'url_for1'       => new Twig_Filter_Function('url_for1'),
-      'url_for'        => new Twig_Filter_Function('url_for'),
-      'link_to'        => new Twig_Filter_Function('link_to'),
-      'url_for_form'   => new Twig_Filter_Function('url_for_form'),
-      'form_tag_for'   => new Twig_Filter_Function('form_tag_for'),
-      'link_to_if'     => new Twig_Filter_Function('link_to_if'),
-      'link_to_unless' => new Twig_Filter_Function('link_to_unless'),
-      'public_path'    => new Twig_Filter_Function('public_path'),
-      'button_to'      => new Twig_Filter_Function('button_to'),
-      'form_tag'       => new Twig_Filter_Function('form_tag'),
-      'mail_to'        => new Twig_Filter_Function('mail_to'),
+      'link_to2'       => new Twig_Function_Function('link_to2', $options),
+      'link_to1'       => new Twig_Function_Function('link_to1', $options),
+      'url_for2'       => new Twig_Function_Function('url_for2', $options),
+      'url_for1'       => new Twig_Function_Function('url_for1', $options),
+      'url_for'        => new Twig_Function_Function('url_for', $options),
+      'link_to'        => new Twig_Function_Function('link_to', $options),
+      'url_for_form'   => new Twig_Function_Function('url_for_form', $options),
+      'form_tag_for'   => new Twig_Function_Function('form_tag_for', $options),
+      'link_to_if'     => new Twig_Function_Function('link_to_if', $options),
+      'link_to_unless' => new Twig_Function_Function('link_to_unless', $options),
+      'public_path'    => new Twig_Function_Function('public_path', $options),
+      'button_to'      => new Twig_Function_Function('button_to', $options),
+      'form_tag'       => new Twig_Function_Function('form_tag', $options),
+      'mail_to'        => new Twig_Function_Function('mail_to', $options),
     );
   }
 
