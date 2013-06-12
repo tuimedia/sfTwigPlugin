b4733c9f8ad40e79adae20d82bc6036df5dbd7c7
Yuriy Berest <djua.com@gmail.com>
2013-06-12 12:24:32 -0400

    Partial_Twig_Extension теперь возвращает ф-и

--- lib/extensions/Partial_Twig_Extension.class.php
+++ lib/extensions/Partial_Twig_Extension.class.php
@@ -18,21 +18,25 @@
  */
 class Partial_Twig_Extension extends Twig_Extension
 {
-    public function getFilters()
+    public function getFunctions()
     {
+        $options = array('is_safe' => array('html'));
+
         return array(
-            'include_component_slot' => new Twig_Filter_Function('include_component_slot'),
-            'get_component_slot' => new Twig_Filter_Function('get_component_slot'),
-            'has_component_slot' => new Twig_Filter_Function('has_component_slot'),
-            'include_component' => new Twig_Filter_Function('include_component'),
-            'get_component' => new Twig_Filter_Function('get_component'),
-            'include_partial' => new Twig_Filter_Function('include_partial'),
-            'get_partial' => new Twig_Filter_Function('get_partial'),
-            'slot' => new Twig_Filter_Function('slot'),
-            'end_slot' => new Twig_Filter_Function('end_slot'),
-            'has_slot' => new Twig_Filter_Function('has_slot'),
-            'include_slot' => new Twig_Filter_Function('include_slot'),
-            'get_slot' => new Twig_Filter_Function('get_slot'),
+            'include_component_slot' => new Twig_Function_Function('include_component_slot', $options),
+            'get_component_slot' => new Twig_Function_Function('get_component_slot', $options),
+            'has_component_slot' => new Twig_Function_Function('has_component_slot'),
+            //
+            'include_component' => new Twig_Function_Function('include_component'),
+            'get_component' => new Twig_Function_Function('get_component', $options),
+            'include_partial' => new Twig_Function_Function('include_partial'),
+            'get_partial' => new Twig_Function_Function('get_partial', $options),
+            //
+            'slot' => new Twig_Function_Function('slot'),
+            'end_slot' => new Twig_Function_Function('end_slot'),
+            'has_slot' => new Twig_Function_Function('has_slot'),
+            'include_slot' => new Twig_Function_Function('include_slot'),
+            'get_slot' => new Twig_Function_Function('get_slot', $options),
         );
     }
 
